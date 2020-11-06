33cc178d47b51dca56cefa901af0a5f9d0b54fd5
NicholasNeto <nicholasneto@outlook.com>
2020-11-05 21:53:10 -0300

    second challenge module 1

--- JavaScript/exercises_module_1.js
+++ JavaScript/exercises_module_1.js
@@ -25,4 +25,26 @@ var buildInfos = (endereco) => {
     return `O usuário mora em ${endereco.cidade} / ${endereco.uf}, no bairro ${endereco.bairro}, na rua ${endereco.rua} com nº ${endereco.numero}`;
 }
 
+console.log('Exercises 1')
 console.log(buildInfos(endereco))
+
+
+/*
+    Crie uma função que dado um intervalo (entre x e y) exiba todos número pares:
+    function pares(x, y) {
+         // código aqui
+    }
+    pares(32, 321);
+*/
+
+
+var getPares = (x, y) => {
+    let lista = []
+    for (let index = x; index < y; index++) {
+        index % 2 === 0 ? lista.push(index) : null
+    }
+    return lista
+}
+
+console.log('Exercises 2')
+console.log(getPares(1, 10))
